f70dae48cab4ad61cf3511e80d7987a26bbc6c09
gilang_permana <51933769+gilangpermana10@users.noreply.github.com>
2022-11-09 23:35:04 +0700

    make change to search bar

--- Tugas7/function.js
+++ Tugas7/function.js
@@ -1,8 +1,14 @@
 const hamburgerButton = document.querySelector(".hamburger");
+const searchButton = document.querySelector(".search-button");
+const searchBtn = document.querySelector(".search-btn");
 const popup = document.querySelector(".popup");
 
 hamburgerButton.addEventListener("click", () => {
   popup.classList.toggle("active");
-  console.log("clicked");
+  
+})
 
+searchButton.addEventListener("click", () => {
+  searchBtn.classList.toggle("active");
+  console.log("clicked");
 })
