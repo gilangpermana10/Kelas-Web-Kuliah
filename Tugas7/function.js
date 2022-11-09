79e430525348529525d826972227f510eb615bc6
gilang_permana <51933769+gilangpermana10@users.noreply.github.com>
2022-11-09 22:28:34 +0700

    change

---
+++ Tugas7/function.js
@@ -0,0 +1,8 @@
+const hamburgerButton = document.querySelector(".hamburger");
+const popup = document.querySelector(".popup");
+
+hamburgerButton.addEventListener("click", () => {
+  popup.classList.toggle("active");
+  console.log("clicked");
+
+})
---
+++ Tugas7/tailwind.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('tailwindcss').Config} **/
+module.exports = {
+  content: ['./*.{html,js}',
+  './index.html',
+  './function.js',
+],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
